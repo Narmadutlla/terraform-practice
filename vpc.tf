df96826575cf34599c78e8ba7b2bdda3fba5c15b
Narmadutlla <Narmadutlla@gmail.com>
2024-07-12 16:14:52 +0530

    add zone and ip

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,8 @@ resource "aws_vpc" "rani-vpc" {
 resource "aws_subnet" "rani-subnet1" {
   vpc_id     = aws_vpc.rani-vpc.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone = "us-west-2"
+  map_public_ip_on_launch = "True" 
   tags = {
     Name = "rani-subnet"
   }
